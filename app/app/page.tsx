1386cfd2970945a30f826c6c14044eac844cc1ef
a <a@adeMacBook-Pro.local>
2025-11-30 20:17:04 +0700

    update suspense

--- app/app/page.tsx
+++ app/app/page.tsx
@@ -1,6 +1,10 @@
-// app/app/page.tsx
+import { Suspense } from "react";
 import PayApp from "@/components/PayApp";
 
 export default function DashboardPage() {
-  return <PayApp />;
+  return (
+    <Suspense fallback={<div className="p-10 text-center">Loading...</div>}>
+      <PayApp />
+    </Suspense>
+  );
 }
